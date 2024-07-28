ab5a897eab4bed665fc462e40dfef55750118793
Priti Aryal <40359121+PritiAryal@users.noreply.github.com>
2024-07-28 07:09:14 +0000

    added storage

---
+++ storage/framework/views/18994b0d80795a1f9d1417cc22944d9f559e4c81.php
@@ -0,0 +1,14 @@
+<?php $__env->startComponent('mail::message'); ?>
+# Welcome to WishComeTrue
+
+This is the community of fellow developers and we love that you have joined us.
+
+
+All the best,<br>
+Priti
+<?php if (isset($__componentOriginal2dab26517731ed1416679a121374450d5cff5e0d)): ?>
+<?php $component = $__componentOriginal2dab26517731ed1416679a121374450d5cff5e0d; ?>
+<?php unset($__componentOriginal2dab26517731ed1416679a121374450d5cff5e0d); ?>
+<?php endif; ?>
+<?php echo $__env->renderComponent(); ?>
+<?php /**PATH /workspaces/Wishgram-/resources/views/emails/welcome-email.blade.php ENDPATH**/ ?>
